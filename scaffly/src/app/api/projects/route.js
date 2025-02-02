500fe2dd930868023b432ae2adb88cb52e97ab11
ludbakazar <ludbakazarr@gmail.com>
2025-02-02 14:22:40 +0700

    Add GET API route for retrieving all projects and implement corresponding model method

--- scaffly/src/app/api/projects/route.js
+++ scaffly/src/app/api/projects/route.js
@@ -1,12 +1,18 @@
 import ProjectModel from "@/db/models/projectModel";
+import { ObjectId } from "mongodb";
 
 export async function POST(req) {
   const { customerId, projectName, projectAddress } = await req.json();
   const newProject = {
-    customerId: customerId,
+    customerId: new ObjectId(customerId),
     projectName: projectName,
     projectAddress: projectAddress,
   };
   await ProjectModel.create(newProject);
   return Response.json({ message: "Project created successfully" });
+}
+
+export async function GET(req) {
+  const projects = await ProjectModel.findAll();
+  return Response.json(projects);
 }
--- scaffly/src/db/models/projectModel.js
+++ scaffly/src/db/models/projectModel.js
@@ -8,6 +8,31 @@ class ProjectModel {
   static async create(project) {
     return this.collection().insertOne(project);
   }
+
+  static async findAll() {
+    const agg = [
+      {
+        $lookup: {
+          from: "customers",
+          localField: "customerId",
+          foreignField: "_id",
+          as: "customerDetail",
+        },
+      },
+      {
+        $project: {
+          customerId: false,
+        },
+      },
+      {
+        $unwind: {
+          path: "$customerDetail",
+          preserveNullAndEmptyArrays: true,
+        },
+      },
+    ];
+    return this.collection().aggregate(agg).toArray();
+  }
 }
 
 export default ProjectModel;
